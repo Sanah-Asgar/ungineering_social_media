1a754ed169ae18d07bf91c9ae32859f6ed59d791
Sanah <sanahasgar786@gmail.com>
2019-07-15 10:48:46 +0530

    databases_queries

---
+++ sql_queries.sql
@@ -0,0 +1,22 @@
+SHOW DATABASES;
+CREATE DATABASE social_media;
+use social_media;
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    college VARCHAR(255),
+    phone_no INT,
+    PRIMARY KEY(ID)
+);
+    
+CREATE TABLE posts(
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    status VARCHAR(255) NOT NULL,
+    time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+    
